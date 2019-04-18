e37be9b5ec3f327d2533d1cc238f3d03a4f19dff
Paula Tulis <tulis.paula@gmail.com>
2019-04-18 16:40:50 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = " "
+  data.each do |season_info, contestant_info|
+    if season_info == season
+  contestant_info.each do |x|
+   if x["status"] == "Winner"
+  winner = x["name"].split
+  winner = winner[0]
+end 
+end 
+end
+end 
+winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-end
+  name_of_contestant = " "
+  data.each do |season_info, contestant_info|
+    contestant_info.each do |x|
+    if x["occupation"] == occupation 
+    name_of_contestant = x["name"]
+    end 
+    end 
+    end 
+    name_of_contestant
+    end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season_info, contestant_info|
+    contestant_info.each do |x|
+      x.each do |a, b|
+    if b == hometown 
+    counter += 1
 end
+end 
+end 
+end
+counter
+end 
 
 def get_occupation(data, hometown)
-  # code here
-end
+  data.each do |season_info, contestant_info|
+    contestant_info.each do |x|
+      x.each do |a, b|
+    if b == hometown
+    return x["occupation"]
+    end 
+    end 
+    end 
+    end 
+  end 
+    
 
-def get_average_age_for_season(data, season)
-  # code here
+def average_age_for(data, season)
+  contestants = data[season]
+  contestants.sum { |contestant| contestant["age"].to_f } / contestants.count
 end
